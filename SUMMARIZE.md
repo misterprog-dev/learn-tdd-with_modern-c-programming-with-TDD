dcf3a4fff58e7fec47275aa2f3215cebd4dbd252
Soumaila DIAKITE <soum.diakite@outlook.com>
2022-09-27 22:36:21 +0200

    c2/Replaces consonants with appropriate digits

--- SUMMARIZE.md
+++ SUMMARIZE.md
@@ -1,3 +1,15 @@
 ## RESUME
-> - Don't let the list of tests force you to do what you do or the order in which you do it. <br>
-> - TDD is a fluid process, and you should generally go where the tests suggest you go next. 
+```
+- Don't let the list of tests force you to do what you do or the order in which you do it.
+- TDD is a fluid process, and you should generally go where the tests suggest you go next. 
+```
+
+```
+- TDD It’s a tool that accommodates continual experimentation, discovery, and refinement.
+
+- When doing TDD, we sometimes make the mistake of being concerned about premature optimizations.
+
+- Durant the implementation the More important is a good design, with consistent interfaces and expressive code.
+
+- Once you finish implementing correct behavior with a solid design, you might or might not consider optimizing performance.
+```
--- tests/chapter2/Soundex.h
+++ tests/chapter2/Soundex.h
@@ -3,14 +3,37 @@
 #include <string>
 
 class Soundex {
+   static const size_t MaxCodeLength{4};
+
    public:
    std::string encode(const std::string& word) const {
-      return zeroPad(word);
+      return zeroPad(head(word) + encodedDigits(word));
+   }
+
+   private:
+   std::string head(const std::string& word) const {
+      return word.substr(0, 1);
+   }
+
+   private:
+   std::string encodedDigits(const std::string& word) const {
+      if (word.length() > 1) return encodedDigit();
+      return "";
+   }
+
+   private:
+   std::string encodedDigit() const {
+      return "1";
    }
 
    private:
    std::string zeroPad(const std::string& word) const {
-      return word + "000";
+      return word + std::string(zerosMissing(word), '0');
+   }
+
+   private:
+   int zerosMissing(const std::string& word) const {
+      return MaxCodeLength - word.length();
    }
 };
 #endif
